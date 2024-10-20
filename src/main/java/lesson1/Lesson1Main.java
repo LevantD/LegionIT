ef5af18f73f263a78bd7a6917c29366cb981add4
LevantD <dlevant.work@gmail.com>
2024-10-20 11:48:51 -0400

    Initial commit

---
+++ src/main/java/lesson1/Lesson1Main.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+public class Lesson1Main {
+    public static void main(String[] args) {
+
+        String txt = "Hello World!";
+        int num = 5;
+        double doubleNum = 5.5;
+        long longNum = 653345564;
+        boolean booleanVar = true;
+        boolean booleanVar2 = false;
+
+        int a = 100;
+        int b = 200;
+
+        double result = (double)a/b;
+
+//        System.out.println(txt);
+//        System.out.println("number = " + num);
+//        System.out.println("double number = " + doubleNum);
+        System.out.println("The result = " + result);
+    }
+}
